c477064545e3c2513ebf212e2d0267146b668323
Maruthi2406 <pavan.b2406@gmail.com>
2025-07-24 12:45:23 +0530

    grocery list

---
+++ src/components/AddItem.js
@@ -0,0 +1,31 @@
+export function AddItem() {
+  return (
+    <>
+      <div>
+        <h3>Add Item</h3>
+      </div>
+      <form className="add-item">
+        <div>
+          <label>Item Name</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Quantity:</label>
+          <select>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+          </select>
+        </div>
+        <div>
+          <label>Price</label>
+          <input type="number" />
+        </div>
+
+        <div>
+          <button className="btn">Add Item</button>
+        </div>
+      </form>
+    </>
+  );
+}
