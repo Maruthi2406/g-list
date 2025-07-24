f5ce5d355d909d31568d8a7ff223949c262f08a0
Maruthi2406 <pavan.b2406@gmail.com>
2025-07-24 15:27:15 +0530

    grocery list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { AddItem } from "./components/AddItem";
 import "./App.css";
+import { ItemList } from "./components/ItemList";
 
 function App() {
   return (
     <div>
       <Header />
       <AddItem />
+      <ItemList />
       <Footer />
     </div>
   );
---
+++ src/components/ItemList.js
@@ -0,0 +1,11 @@
+import SingleItem from "./SingleItem";
+
+export function ItemList() {
+  return (
+    <div>
+      <SingleItem />
+      <SingleItem />
+      <SingleItem />
+    </div>
+  );
+}
