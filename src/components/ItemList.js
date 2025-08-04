ec46fe7ba62d960a1c52892fbb7efe4c4c18985d
Maruthi2406 <pavan.b2406@gmail.com>
2025-08-04 19:16:50 +0530

    derived state and reduce method

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,12 +1,18 @@
 import SingleItem from "./SingleItem";
 
 export function ItemList({ items }) {
+  const total = items.reduce((accum, item) => {
+    return (accum = accum + item.price * item.quantity);
+  }, 0);
+  if (!items.length)
+    return <h3 style={{ textAlign: "center" }}>Please add items</h3>;
   return (
     <div>
       <h3>Items List</h3>
       {items.map((item) => (
         <SingleItem item={item} key={item.id} />
       ))}
+      <div className="total"> Total:{total} </div>
     </div>
   );
 }
